79dcf75f6741d398d3bddebfdc74eb00fb9fb352
Hannes Weichelt <hweichelt@uni-potsdam.de>
2025-09-03 21:53:50 +0200

    First register view

--- src/main.py
+++ src/main.py
@@ -1,7 +1,29 @@
+import requests
+
 import flet as ft
+from flet.core.control_event import ControlEvent
+
+BACKEND_URL = "http://localhost:5150"
+
+
+def register(event: ControlEvent, name: str, email: str, password: str) -> None:
+    register_url = f"{BACKEND_URL}/api/auth/register"
+    headers = {"Content-Type": "application/json"}
+    request_data = {
+        "name": name,
+        "email": email,
+        "password": password,
+    }
+    requests.post(register_url, headers=headers, json=request_data)
 
 
 def main(page: ft.Page):
+    input_username = ft.TextField(label="Username")
+    input_email = ft.TextField(label="E-Mail")
+    input_password = ft.TextField(label="Password", password=True, can_reveal_password=True)
+    button_register = ft.ElevatedButton("Register", on_click=lambda e: register(e, input_username.value, input_email.value, input_password.value))
+
+
     counter = ft.Text("0", size=50, data=0)
 
     def increment_click(e):
@@ -14,11 +36,14 @@ def main(page: ft.Page):
     )
     page.add(
         ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
-            ),
-            expand=True,
+            ft.Column(
+                [
+                    input_username,
+                    input_email,
+                    input_password,
+                    button_register
+                ]
+            )
         )
     )
 
