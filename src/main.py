74b223a3f2ff369f46ce20b2cc1723e8fdce749a
Hannes Weichelt <hannes.weichelt@ostech.de>
2025-09-04 09:38:10 +0200

    Removed template controls from main

--- src/main.py
+++ src/main.py
@@ -23,17 +23,6 @@ def main(page: ft.Page):
     input_password = ft.TextField(label="Password", password=True, can_reveal_password=True)
     button_register = ft.ElevatedButton("Register", on_click=lambda e: register(e, input_username.value, input_email.value, input_password.value))
 
-
-    counter = ft.Text("0", size=50, data=0)
-
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
-
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
     page.add(
         ft.SafeArea(
             ft.Column(
