49822c8e269c753c971b084dccaf998ec9da926e
Hannes Weichelt <hweichelt@uni-potsdam.de>
2025-12-06 16:41:16 +0100

    Added login view to app

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,6 +1,7 @@
 from .base import View
 from .view_base import ViewBase
 from .view_register import ViewRegister
+from .view_login import ViewLogin
 from .manager import ViewManager
 
-__all__ = ["View", "ViewBase", "ViewManager", "ViewRegister"]
+__all__ = ["View", "ViewBase", "ViewManager", "ViewRegister","ViewLogin"]
